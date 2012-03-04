f4e0d2d0a1c791499e327342d5c5a6e552224662
Salizer <rene_thomsen@yahoo.com>
2012-03-04 18:03:02 +0100

    Lagt fil op for testning af Field.
    Mangler implementation af tests.

---
+++ SWT_Ex6_RouletteGame/RouletteGame.Tests/FieldTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace RouletteGame.Tests
+{
+    [TestFixture]
+    class FieldTests
+    {
+        // Members
+        private Field fTestObj;
+        private uint uiNumber;
+        private uint uiColor;
+        private bool bEven;
+
+        // Setup and teardown
+        [SetUp]
+        public void setup()
+        {
+            uiNumber = 2;
+            uiColor = Field.Black;
+            bEven = true;
+            fTestObj = new Field(uiNumber, uiColor);
+        }
+        [TearDown]
+        public void teardown()
+        {
+            fTestObj = null;   
+        }
+
+        // Tests
+        [Test]
+        public void Color_SetsColorGreen_IsNowGreen()
+        {
+            
+        }
+        [Test]
+        public void Color_SetsColorBlack_IsNowBlack()
+        {
+            
+        }
+        [Test]
+        public void Color_SetsColorRed_IsNowRed()
+        {
+            
+        }
+        [Test]
+        public void Number_ValidNumber_IsSaved()
+        {
+            
+        }
+        [Test]
+        public void Number_InValidNumber_ThrowsException()
+        {
+            
+        }
+        [Test]
+        public void Even_EvenNumber_ReturnsTrue()
+        {
+            
+        }
+        [Test]
+        public void Even_NotEvenNumber_ReturnsFalse()
+        {
+            
+        }
+    }
+}
